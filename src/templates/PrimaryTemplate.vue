b0079908dea97c6888b469e6412235a04f2c671b
umn <langl269@umn.edu>
2024-10-29 21:29:52 -0500

    Added routing, created a couple of dummy routes.

---
+++ src/templates/PrimaryTemplate.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="primary-template">
+    <header>My Students!</header>
+
+    <section class="main">
+      <slot></slot>
+    </section>
+
+    <footer>
+      <div class="contents">
+        <div>Go to top of page</div>
+        <div>This year</div>
+      </div>
+    </footer>
+  </section>
+</template>
+
+<style scoped>
+section.primary-template {
+  section.main {
+    max-width: 800px;
+    margin: 0 auto;
+    border: 1px solid red;
+  }
+}
+</style>
